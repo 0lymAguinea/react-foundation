caf52e5d6eb04b4ebeebbca5cf0dc48d1c40244b
0lym <aguineaolymm@gmail.com>
2024-02-05 19:11:40 +0800

    Added form components

--- CVApplication/src/components/CVForm.jsx
+++ CVApplication/src/components/CVForm.jsx
@@ -1,4 +1,36 @@
-function CVForm() {
-  return <section>CVForm</section>;
+import Form from "react-bootstrap/Form";
+
+function CreateFormLabel() {
+  return <Form.Label></Form.Label>;
 }
+
+function CreateFormInput() {
+  return <Form.Control></Form.Control>;
+}
+
+function CreateFormGroup() {
+  return (
+    <Form.Group>
+      <CreateFormLabel />
+      <CreateFormInput />
+    </Form.Group>
+  );
+}
+
+function CreateForm() {
+  return (
+    <Form>
+      <CreateFormGroup />
+    </Form>
+  );
+}
+
+function CVForm() {
+  return (
+    <section>
+      <CreateForm> </CreateForm>
+    </section>
+  );
+}
+
 export default CVForm;
