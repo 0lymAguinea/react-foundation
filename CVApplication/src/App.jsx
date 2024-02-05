b29cd9ae5d9f005aada3c2cc015d4e6117d5283c
0lym <aguineaolymm@gmail.com>
2024-02-05 18:01:36 +0800

    Added layout to sections

--- CVApplication/src/App.jsx
+++ CVApplication/src/App.jsx
@@ -1,13 +1,25 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import Header from "./components/Header";
 import CVForm from "./components/CVForm";
 import CVPreview from "./components/CVPreview";
 function App() {
   return (
-    <>
-      <Header />
-      <CVForm />
-      <CVPreview />
-    </>
+    <Container fluid>
+      <Row>
+        <Header />
+      </Row>
+      <Row>
+        <Col>
+          <CVForm />
+        </Col>
+        <Col>
+          <CVPreview />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
