e158ee4168efd81f8c8b4c0983cf7d74afc04b09
0lym <aguineaolymm@gmail.com>
2024-02-07 17:04:22 +0800

    Added all inputs to be inputted at paragraph

--- CVApplication/src/components/CVPreview.jsx
+++ CVApplication/src/components/CVPreview.jsx
@@ -1,22 +1,131 @@
-function CVPreview({ inputValue }) {
+/* eslint-disable react/prop-types */
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "../styles/CVPreview.css";
+
+function TextOutputMajorDegree({ degree, major }) {
+  return (
+    <span>
+      {degree} in {major}
+    </span>
+  );
+}
+
+function TextOutputWithTitle({ title, inputValue }) {
+  return (
+    <p>
+      {title} {inputValue}
+    </p>
+  );
+}
+
+function TextOutput({ inputValue }) {
+  return <p>{inputValue}</p>;
+}
+
+function PreviewBody({ inputValue }) {
   return (
     <>
-      <input type="text" value={inputValue.firstName} readOnly disabled />
-      <input type="text" value={inputValue.lastName} readOnly />
-      <input type="text" value={inputValue.emailAddress} readOnly />
-      <input type="text" value={inputValue.mobileNumber} readOnly />
+      <Row className="reduceBottomMargin">
+        <Col className="fullName text-end ">
+          <TextOutput inputValue={inputValue.firstName} />
+        </Col>
+        <Col className="fullName">
+          <TextOutput inputValue={inputValue.lastName} />
+        </Col>
+      </Row>
 
-      <input type="text" value={inputValue.universityName} readOnly />
-      <input type="text" value={inputValue.graduateYear} readOnly />
-      <input type="text" value={inputValue.degreeName} readOnly />
-      <input type="text" value={inputValue.majorName} readOnly />
-
-      <input type="text" value={inputValue.companyName} readOnly />
-      <input type="text" value={inputValue.position} readOnly />
-      <input type="text" value={inputValue.achievements} readOnly />
-      <input type="text" value={inputValue.workDurationFrom} readOnly />
-      <input type="text" value={inputValue.workDurationUntil} readOnly />
+      <Row className="contactInfos mx-5">
+        <Col className="text-end">
+          <TextOutput inputValue={inputValue.emailAddress} />
+        </Col>
+        <Col className="text-center">
+          <TextOutput inputValue={inputValue.mobileNumber} />
+        </Col>
+        <Col className="text-start">
+          <TextOutput inputValue={inputValue.github} />
+        </Col>
+      </Row>
+      <hr className="mt-0"></hr>
+      <h3>Technical skills</h3>
+      <Row>
+        <TextOutputWithTitle
+          title="Programming languages: "
+          inputValue={inputValue.languages}
+        />
+        <TextOutputWithTitle
+          title="Technologies / Frameworks: "
+          inputValue={inputValue.technologiesFrameworks}
+        />
+        <TextOutputWithTitle
+          title="Project management: "
+          inputValue={inputValue.projectManagement}
+        />
+        <TextOutputWithTitle
+          title="Developer tools: "
+          inputValue={inputValue.developerTools}
+        />
+      </Row>
+      <hr />
+      <h3>Education</h3>
+      <Row>
+        <Col xl={9}>
+          <TextOutput inputValue={inputValue.universityName} />
+        </Col>
+        <Col xl={3} className="text-end mx-0">
+          <Row>
+            <Col className="text-end">
+              <TextOutput inputValue={inputValue.yearStart} />
+            </Col>
+            -
+            <Col className="text-start">
+              <TextOutput inputValue={inputValue.yearGraduate} />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <TextOutputMajorDegree
+          degree={inputValue.degreeName}
+          major={inputValue.majorName}
+        />
+      </Row>
+      <hr />
+      <h3>Work experience</h3>
+      <Row>
+        <Col xl={9}>
+          <TextOutput inputValue={inputValue.companyName} />
+          <TextOutput inputValue={inputValue.position} />
+        </Col>
+        <Col xl={3} className="text-end mx-0">
+          <Row>
+            <Col className="text-end">
+              <TextOutput inputValue={inputValue.workDurationFrom} />
+            </Col>
+            -
+            <Col className="text-start ">
+              <TextOutput inputValue={inputValue.workDurationUntil} />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-end">
+          <TextOutput inputValue={inputValue.workDurationFrom} />
+        </Col>
+        -
+        <Col className="text-start ">
+          <TextOutput inputValue={inputValue.workDurationUntil} />
+        </Col>
+      </Row>
+      <Row>
+        <TextOutput inputValue={inputValue.achievements} />
+      </Row>
     </>
   );
+}
+
+function CVPreview({ inputValue }) {
+  return <PreviewBody inputValue={inputValue} />;
 }
 export default CVPreview;
