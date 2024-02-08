ba4c69061fd7a3d1d2f161dcd33bea838b750c45
0lym <aguineaolymm@gmail.com>
2024-02-08 13:18:08 +0800

    Added form validation feedback

--- CVApplication/src/components/CVForm.jsx
+++ CVApplication/src/components/CVForm.jsx
@@ -5,6 +5,7 @@ import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 
 import "../styles/CVForm.css";
+import { useState } from "react";
 
 function SectionTitle({ title }) {
   return <h3 className="sectionTitle">{title}</h3>;
@@ -14,7 +15,14 @@ function CreateFormLabel({ label }) {
   return <Form.Label className="label">{label}</Form.Label>;
 }
 
-function CreateFormInput({ inputType, label, inputValue, handleChange, as }) {
+function CreateFormInput({
+  inputType,
+  label,
+  inputValue,
+  handleChange,
+  as,
+  pattern,
+}) {
   return (
     <>
       <Form.Control
@@ -23,6 +31,7 @@ function CreateFormInput({ inputType, label, inputValue, handleChange, as }) {
         value={inputValue}
         onChange={handleChange}
         as={as}
+        pattern={pattern}
         required
       ></Form.Control>
     </>
@@ -35,7 +44,9 @@ function CreateFormGroup({
   inputValue,
   handleChange,
   groupID,
+  feedbackMessage,
   as,
+  pattern,
 }) {
   return (
     <Form.Group controlId={groupID}>
@@ -46,14 +57,29 @@ function CreateFormGroup({
         inputValue={inputValue}
         handleChange={handleChange}
         as={as}
+        pattern={pattern}
       />
+      <Form.Control.Feedback type="invalid">
+        {feedbackMessage}
+      </Form.Control.Feedback>
     </Form.Group>
   );
 }
 
 function CreateForm({ title, inputValue, handleChange }) {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+  };
   return (
-    <Form>
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <SectionTitle title={title[0]} />
       <section>
         <CreateFormGroup
@@ -62,6 +88,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.firstName}
           handleChange={(e) => handleChange("firstName", e.target.value)}
           groupID="firstName"
+          feedbackMessage="Please enter your first name."
         />
         <CreateFormGroup
           inputType="text"
@@ -69,6 +96,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.lastName}
           handleChange={(e) => handleChange("lastName", e.target.value)}
           groupID="lastName"
+          feedbackMessage="Please enter your last name."
         />
         <CreateFormGroup
           inputType="email"
@@ -76,6 +104,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.emailAddress}
           handleChange={(e) => handleChange("emailAddress", e.target.value)}
           groupID="emailAddress"
+          feedbackMessage="Please enter a valid email address."
         />
         <CreateFormGroup
           inputType="tel"
@@ -83,6 +112,8 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.mobileNumber}
           handleChange={(e) => handleChange("mobileNumber", e.target.value)}
           groupID="mobileNumber"
+          feedbackMessage="Please enter a valid mobile number."
+          pattern="^\d{4,13}$"
         />
         <CreateFormGroup
           inputType="text"
@@ -90,6 +121,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.github}
           handleChange={(e) => handleChange("github", e.target.value)}
           groupID="github"
+          feedbackMessage="Please enter your GitHub username."
         />
       </section>
 
@@ -101,6 +133,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.languages}
           handleChange={(e) => handleChange("languages", e.target.value)}
           groupID="languages"
+          feedbackMessage="Please list the programming languages you know."
         />
         <CreateFormGroup
           inputType="text"
@@ -110,6 +143,7 @@ function CreateForm({ title, inputValue, handleChange }) {
             handleChange("technologiesFrameworks", e.target.value)
           }
           groupID="technologiesFrameworks"
+          feedbackMessage="Please list the technologies or frameworks you are familiar with."
         />
         <CreateFormGroup
           inputType="text"
@@ -117,6 +151,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.developerTools}
           handleChange={(e) => handleChange("developerTools", e.target.value)}
           groupID="developerTools"
+          feedbackMessage="Please list the developer tools you use."
         />
         <CreateFormGroup
           inputType="text"
@@ -126,6 +161,7 @@ function CreateForm({ title, inputValue, handleChange }) {
             handleChange("projectManagement", e.target.value)
           }
           groupID="projectManagement"
+          feedbackMessage="Please list the project management tools you are experienced with."
         />
       </section>
 
@@ -137,6 +173,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.universityName}
           handleChange={(e) => handleChange("universityName", e.target.value)}
           groupID="universityName"
+          feedbackMessage="Please enter the name of your university."
         />
         <Row>
           <Col>
@@ -146,6 +183,7 @@ function CreateForm({ title, inputValue, handleChange }) {
               inputValue={inputValue.graduateYear}
               handleChange={(e) => handleChange("yearStart", e.target.value)}
               groupID="yearStart"
+              feedbackMessage="Please enter the start year of your education."
             />
           </Col>
           <Col>
@@ -155,6 +193,7 @@ function CreateForm({ title, inputValue, handleChange }) {
               inputValue={inputValue.graduateYear}
               handleChange={(e) => handleChange("yearGraduate", e.target.value)}
               groupID="yearGraduate"
+              feedbackMessage="Please enter the end year of your education."
             />
           </Col>
         </Row>
@@ -164,6 +203,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.degreeName}
           handleChange={(e) => handleChange("degreeName", e.target.value)}
           groupID="degreeName"
+          feedbackMessage="Please enter your degree."
         />
         <CreateFormGroup
           inputType="text"
@@ -171,6 +211,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.majorName}
           handleChange={(e) => handleChange("majorName", e.target.value)}
           groupID="majorName"
+          feedbackMessage="Please enter your major."
         />
       </section>
 
@@ -182,6 +223,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.companyName}
           handleChange={(e) => handleChange("companyName", e.target.value)}
           groupID="companyName"
+          feedbackMessage="Please enter the name of the company."
         />
         <CreateFormGroup
           inputType="text"
@@ -189,6 +231,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.position}
           handleChange={(e) => handleChange("position", e.target.value)}
           groupID="position"
+          feedbackMessage="Please enter your position at the company."
         />
         <CreateFormGroup
           inputType=""
@@ -196,6 +239,7 @@ function CreateForm({ title, inputValue, handleChange }) {
           inputValue={inputValue.achievements}
           handleChange={(e) => handleChange("achievements", e.target.value)}
           groupID="achievements"
+          feedbackMessage="Please enter your achievement at the company."
           as="textarea"
         />
         <Row className="workDuration">
@@ -208,6 +252,7 @@ function CreateForm({ title, inputValue, handleChange }) {
                 handleChange("workDurationFrom", e.target.value)
               }
               groupID="workDurationFrom"
+              feedbackMessage="Please enter the start date of your employment."
             />
           </Col>
           <Col>
@@ -219,10 +264,12 @@ function CreateForm({ title, inputValue, handleChange }) {
                 handleChange("workDurationUntil", e.target.value)
               }
               groupID="workDurationUntil"
+              feedbackMessage="Please enter the end date of your employment."
             />
           </Col>
         </Row>
       </section>
+      <Button type="submit">Submit</Button>
     </Form>
   );
 }
