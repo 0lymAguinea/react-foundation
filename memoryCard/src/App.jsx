bad2567b0be92a873125497844347b9150c36c83
0lym <aguineaolymm@gmail.com>
2024-02-13 14:37:37 +0800

    Added modal and board game components

--- memoryCard/src/App.jsx
+++ memoryCard/src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/main.scss";
+import { Container } from "react-bootstrap";
+import HowToPlay from "./components/HowToPlay";
+import BoardGame from "./components/BoardGame";
+
+function App() {
+  return (
+    <Container fluid>
+      <HowToPlay />
+      <BoardGame />
+    </Container>
+  );
+}
 
 export default App;
---
+++ memoryCard/src/components/HowToPlay.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+function ModalInfo(props) {
+  return (
+    <Modal show={props.show} onHide={props.handleHide} centered>
+      <Modal.Header>
+        <Modal.Title>How to play?</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Click a pokemon to get points but don&apos;t click a pokemon twice!
+      </Modal.Body>
+      <Modal.Footer className="mx-auto">
+        <Button variant="success" onClick={props.handleHide}>
+          Start game
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function HowToPlay() {
+  const [show, setShow] = useState(true);
+  const handleHide = () => {
+    setShow(false);
+  };
+  return <ModalInfo show={show} handleHide={handleHide}></ModalInfo>;
+}
+export default HowToPlay;
