c1626213ebbae2d5b227f28667543ab4ade13b1a
0lym <aguineaolymm@gmail.com>
2024-02-13 17:04:10 +0800

    Added function that uses pokeAPI to fetch 100 random pokemon names

--- memoryCard/src/components/BoardGame.jsx
+++ memoryCard/src/components/BoardGame.jsx
@@ -1,5 +1,53 @@
-import { useState } from "react";
-import { Button, Col, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Col, Row, Card } from "react-bootstrap";
+
+function MainContent() {
+  const [pokemonName, setPokemonName] = useState([]);
+
+  useEffect(() => {
+    async function getPokemon() {
+      const max = 100;
+      const min = 1;
+      const pokemonSet = new Set();
+
+      while (pokemonSet.size < 12) {
+        let randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${randomNum}`,
+          { method: "GET" }
+        );
+        const data = await response.json();
+        const pokemon = data.name;
+        pokemonSet.add(pokemon);
+      }
+      setPokemonName(Array.from(pokemonSet));
+    }
+    getPokemon();
+  }, []);
+  console.log(pokemonName);
+  return (
+    <main>
+      <Row>
+        {pokemonName.slice(0, 6).map((pokemon) => (
+          <Col sm={2} key={pokemon}>
+            <Card>
+              <Card.Body>{pokemon}</Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      <Row>
+        {pokemonName.slice(6, 12).map((pokemon) => (
+          <Col sm={2} key={pokemon}>
+            <Card>
+              <Card.Body>{pokemon}</Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </main>
+  );
+}
 
 function Header({ score, highestScore }) {
   return (
@@ -39,8 +87,7 @@ function BoardGame() {
   return (
     <>
       <Header score={score} highestScore={highestScore} />
-      <Button onClick={handleScoreIncrease}>ADD SCORE</Button>
-      <Button onClick={handleScoreReset}>RESET SCORE</Button>
+      <MainContent />
     </>
   );
 }
