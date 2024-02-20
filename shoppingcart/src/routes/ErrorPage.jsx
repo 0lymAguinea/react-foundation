39d6ebf6d4764eaa784051ebc99399636e5330f9
Olym <aguineaolymm@gmail.com>
2024-02-20 17:42:19 +0800

    Added basic error element

--- shoppingcart/src/routes/ErrorPage.jsx
+++ shoppingcart/src/routes/ErrorPage.jsx
@@ -1,2 +1,11 @@
-function ErrorPage() {}
+import { Link } from "react-router-dom";
+
+function ErrorPage() {
+  return (
+    <>
+      <h1>This page doesn&apos;t exist.</h1>
+      <Link to="/">Click here to go back to home page</Link>
+    </>
+  );
+}
 export default ErrorPage;
