bec8af4b95c85f0d8d79e2058289b271adc65dac
Olym <aguineaolymm@gmail.com>
2024-02-20 23:40:46 +0800

    Added informations to home page

--- shoppingcart/src/components/Home.jsx
+++ shoppingcart/src/components/Home.jsx
@@ -1,4 +1,26 @@
+import { Link } from "react-router-dom";
+import "../styles/home.css";
+import { Button } from "react-bootstrap";
+import { IoEnter } from "react-icons/io5";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <main className="">
+      <h1 className="">
+        Welcome to <span className="brand">ShopHere</span>
+      </h1>
+      <p className="fs-5 ">
+        {" "}
+        Where Every Click Leads to a Happy Checkout! Discover the Joy of
+        Seamless Shopping Today!
+      </p>
+      <Link to="shop" className="">
+        <Button type="button" className="">
+          Start shopping
+          <IoEnter className="ms-2" />{" "}
+        </Button>
+      </Link>
+    </main>
+  );
 }
 export default Home;
