0e85014973d37d991b7355c586a629391804134a
Olym <aguineaolymm@gmail.com>
2024-02-20 17:41:35 +0800

    Configure routes. Adding shop page as children

--- shoppingcart/src/App.jsx
+++ shoppingcart/src/App.jsx
@@ -1,6 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <Navbar />
+      <Container fluid>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- shoppingcart/src/routes/Router.jsx
+++ shoppingcart/src/routes/Router.jsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "./ErrorPage";
+import Shop from "../components/Shop";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: "*",
       element: <App />,
       errorElement: <ErrorPage />,
+      children: [{ path: "shop", element: <Shop /> }],
     },
   ]);
 
