dd5b0e25bdbac6a816cf34be51d6b07f5c0a7a44
Olym <aguineaolymm@gmail.com>
2024-02-24 00:11:53 +0800

    Create cart state and pass the setter to each item button in the Shop component child

--- shoppingcart/src/App.jsx
+++ shoppingcart/src/App.jsx
@@ -1,18 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
-
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (props) => {
+    setCart([...cart, props]);
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar cart={cart} />
       <Container fluid>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
+          <Route
+            path="/shop"
+            element={<Shop handleAddToCart={handleAddToCart} />}
+          />
         </Routes>
       </Container>
     </>
