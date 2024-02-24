b310d5171aab07765a63510184e492a3986ca839
Olym <aguineaolymm@gmail.com>
2024-02-24 19:04:02 +0800

    Added handler for adding items to cart

--- shoppingcart/src/App.jsx
+++ shoppingcart/src/App.jsx
@@ -5,11 +5,36 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
+import Cart from "./components/Cart";
 function App() {
   const [cart, setCart] = useState([]);
+  const [productId, setProductId] = useState([]);
 
   const handleAddToCart = (props) => {
-    setCart([...cart, props]);
+    const newItem = {
+      id: props.id,
+      title: props.title,
+      props: props.price,
+      description: props.description,
+      category: props.category,
+      image: props.image,
+      rating: props.rating,
+      quantity: 1,
+    };
+
+    if (productId.includes(newItem.id)) {
+      setCart((prevCart) => {
+        const updatedCart = prevCart.map((item) =>
+          item.id === newItem.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+        return updatedCart;
+      });
+    } else {
+      setProductId((prevIds) => [...prevIds, newItem.id]);
+      setCart((prevItem) => [...prevItem, newItem]);
+    }
   };
 
   return (
@@ -22,6 +47,7 @@ function App() {
             path="/shop"
             element={<Shop handleAddToCart={handleAddToCart} />}
           />
+          <Route path="/cart" element={<Cart cart={cart} />} />
         </Routes>
       </Container>
     </>
