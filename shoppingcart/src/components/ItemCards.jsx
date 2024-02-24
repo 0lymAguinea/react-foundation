389394657fd930149e3f0d496dfa9b0775762bf8
Olym <aguineaolymm@gmail.com>
2024-02-25 01:44:14 +0800

    Create quantity counter state. Add a input and 2 buttons to each card.

--- shoppingcart/src/components/ItemCards.jsx
+++ shoppingcart/src/components/ItemCards.jsx
@@ -1,8 +1,28 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row, InputGroup, Form } from "react-bootstrap";
 import PropTypes from "prop-types";
+import { useState } from "react";
 import "../styles/card.css";
+
 import { oneOfType } from "prop-types";
 function ItemsCards(props) {
+  let [itemCounter, setItemCounter] = useState(1);
+
+  const handleCounterInput = (e) => {
+    const inputValue = e.target.value;
+
+    setItemCounter(inputValue);
+  };
+
+  const handleCounterIncrement = () => {
+    setItemCounter(parseInt(itemCounter) + 1);
+  };
+
+  const handleCounterDecrement = () => {
+    if (itemCounter > 1) {
+      setItemCounter(itemCounter - 1);
+    }
+  };
+
   const itemInfo = {
     id: props.id,
     title: props.title,
@@ -11,6 +31,7 @@ function ItemsCards(props) {
     category: props.category,
     image: props.image,
     rating: props.rating,
+    quantity: itemCounter,
   };
   return (
     <Card className="mb-4">
@@ -24,13 +45,28 @@ function ItemsCards(props) {
         <Card.Text>
           <span>$</span> {props.price}
         </Card.Text>
-        <Button
-          type="button"
-          className="float-right"
-          onClick={() => props.handleAddToCart(itemInfo)}
-        >
-          Add to Cart
-        </Button>
+        <Row>
+          <InputGroup>
+            <Button onClick={handleCounterDecrement}>-</Button>
+            <Form.Control
+              id={props.id}
+              type="number"
+              value={itemCounter}
+              min={1}
+              max={50}
+              onChange={(e) => handleCounterInput(e)}
+            ></Form.Control>
+            <Button onClick={handleCounterIncrement}>+</Button>
+          </InputGroup>
+          <Button
+            type="button"
+            className="float-right"
+            onClick={() => props.handleAddToCart(itemInfo)}
+            disabled={itemCounter === "" || itemCounter === "0"}
+          >
+            Add to Cart
+          </Button>
+        </Row>
       </Card.Body>
     </Card>
   );
@@ -45,5 +81,6 @@ ItemsCards.propTypes = {
   category: PropTypes.string,
   rating: PropTypes.object,
   handleAddToCart: PropTypes.func,
+  quantity: PropTypes.number,
 };
 export default ItemsCards;
