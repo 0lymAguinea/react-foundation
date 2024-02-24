fed824e0c5efe6fd8e4773277441f4c2f7757f5a
Olym <aguineaolymm@gmail.com>
2024-02-24 19:05:23 +0800

    Added Cart component to router

--- shoppingcart/src/routes/Router.jsx
+++ shoppingcart/src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "./ErrorPage";
 import Shop from "../components/Shop";
+import Cart from "../components/Cart";
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -9,7 +10,10 @@ const Router = () => {
       path: "*",
       element: <App />,
       errorElement: <ErrorPage />,
-      children: [{ path: "shop", element: <Shop /> }],
+      children: [
+        { path: "shop", element: <Shop /> },
+        { path: "cart", element: <Cart /> },
+      ],
     },
   ]);
 
