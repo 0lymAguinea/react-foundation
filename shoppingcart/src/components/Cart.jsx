cc95ed64178dc4dfdb2bd5aa564986a2715827f3
Olym <aguineaolymm@gmail.com>
2024-02-25 22:15:59 +0800

    Modify Cart component to have 2 sides. Added left side part with style

--- shoppingcart/src/components/Cart.jsx
+++ shoppingcart/src/components/Cart.jsx
@@ -1,26 +1,43 @@
-import { Row, Col } from "react-bootstrap";
-function Cart({ cart }) {
-  let colSize = 12;
-  if (cart.length === 0) {
-    return <h1>NO ITEMS</h1>;
-  } else if (cart.length === 1) {
-    colSize = 12;
-  } else if (cart.length === 2) {
-    colSize = 6;
-  } else if (cart.length === 3) {
-    colSize = 4;
-  } else {
-    colSize = 3;
-  }
+import { Container, Row, Col } from "react-bootstrap";
+import "../styles/cart.css";
 
+function ItemList({ item }) {
+  const total = item.price * item.quantity;
   return (
-    <Row>
-      {cart.map((product) => (
-        <Col key={product.id} md={colSize}>
-          {product.title} : Quantity {product.quantity}
-        </Col>
-      ))}
+    <Row className="my-4 itemCheckoutContainer border border-dark-subtle">
+      <Col className="itemImageContainer my-2">
+        <img
+          src={item.image}
+          className="float-start img-thumbnail cartImages me-2"
+        />
+      </Col>
+      <Col>
+        <p className="fs-5">{item.title}</p>
+        <p className="">Quantity: {item.quantity}</p>
+        <p className="">Price: ${item.price}</p>
+        <p>Total: ${total}</p>
+      </Col>
     </Row>
   );
 }
+
+function Cart({ cart }) {
+  console.log(cart);
+  if (cart.length === 0) {
+    return <h1>NO ITEMS</h1>;
+  }
+  return (
+    <Container>
+      <Row>
+        <Col md={7}>
+          {cart.map((item) => (
+            <ItemList key={item.id} item={item} />
+          ))}
+        </Col>
+        <Col md={5}>TOTAL</Col>
+      </Row>
+    </Container>
+  );
+}
+
 export default Cart;
