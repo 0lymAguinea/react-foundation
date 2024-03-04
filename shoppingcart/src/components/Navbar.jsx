cb8888d4195253e26258da4819793078fadb8bd5
0lym <aguineaolymm@gmail.com>
2024-03-04 17:53:44 +0800

    Added state that takes screen width to modify the placement of cart icon

--- shoppingcart/src/components/Navbar.jsx
+++ shoppingcart/src/components/Navbar.jsx
@@ -4,8 +4,23 @@ import Navbar from "react-bootstrap/Navbar";
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
 import { IoMdCart } from "react-icons/io";
+import { useState, useEffect } from "react";
 
 function Header(props) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -15,20 +30,45 @@ function Header(props) {
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto align-items-center text-center">
-            <Link to="/" className="fs-2 nav-link">
-              Home
+        {windowWidth < 992 ? (
+          <>
+            <Link to="cart" aria-label="Cart pag" className="ms-2">
+              <IoMdCart className="fs-2 my-auto position-relative" alt="" />
+              <span className="position-absolute  translate-middle badge border border-light rounded-circle bg-danger p-2">
+                {props.cart.length}
+              </span>
             </Link>
-            <Link to="shop" className="fs-2 nav-link">
-              Shop
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto align-items-center text-center">
+                <Link to="/" className="fs-2 nav-link">
+                  Home
+                </Link>
+                <Link to="shop" className="fs-2 nav-link">
+                  Shop
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </>
+        ) : (
+          <>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto align-items-center text-center">
+                <Link to="/" className="fs-2 nav-link">
+                  Home
+                </Link>
+                <Link to="shop" className="fs-2 nav-link">
+                  Shop
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+            <Link to="cart" aria-label="Cart pag" className="ms-1">
+              <IoMdCart className="fs-2 my-auto position-relative" alt="" />
+              <span className="position-absolute  translate-middle badge border border-light rounded-circle bg-danger p-2">
+                {props.cart.length}
+              </span>
             </Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Link to="cart" aria-label="Cart page">
-          <IoMdCart className="fs-2 my-auto" alt="" />
-        </Link>
-        <span>{props.cart.length}</span>
+          </>
+        )}
       </Container>
     </Navbar>
   );
